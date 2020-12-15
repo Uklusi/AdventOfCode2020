81b0caa1c33be8371dd7c6913d655a1ce170c61f
Manuele Cusumano <mcusumano@KPMG.IT>
2020-12-15 08:53:11 +0100

    Part 15

---
+++ Part_15/part1.py
@@ -0,0 +1,37 @@
+result = 0
+
+revDict = {}
+seq = []
+count = 0
+
+with open("input.txt", "r") as input:
+    line = input.readline().strip()
+    seq = [int(n) for n in line.split(",")]
+
+for n in seq:
+    count += 1
+    revDict[n] = [count]
+    prevNum = n
+    # print(prevNum)
+
+def insertLastNum(n, count, prevPos):
+    if n not in prevPos:
+        prevPos[n] = [count]
+    else:
+        prevPos[n] = [count, prevPos[n][0]]
+
+while count < 2020:
+    count += 1
+    n = prevNum
+    if len(revDict[n]) == 1:
+        prevNum = 0
+    else:
+        prevNum = revDict[n][0] - revDict[n][1]
+    # print(prevNum)
+    insertLastNum(prevNum, count, revDict)
+
+result = prevNum
+
+with open("output1.txt", "w") as output:
+    output.write(str(result))
+    print(str(result))
