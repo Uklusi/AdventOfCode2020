90378e31238024fab83b993f8fec4ce39439dfa4
Manuele Cusumano <mcusumano@KPMG.IT>
2020-12-17 10:05:44 +0100

    Day 17

---
+++ Day_17/part1.py
@@ -0,0 +1,53 @@
+result = 0
+
+active = []
+
+y = 0
+z = 0
+with open("input.txt", "r") as input:
+    for line in input:
+        line = line.strip()
+        l = len(line)
+        for x in range(l):
+            if line[x] == "#":
+                active.append((x, y, z))
+        y -= 1
+
+toll = (-1, 0, 1)
+
+
+def neighbours(pos):
+    return [
+        (pos[0] + a, pos[1] + b, pos[2] + c)
+        for a in toll for b in toll for c in toll
+        if (a, b, c) != (0, 0, 0)
+    ]
+
+
+def isAlive(pos):
+    return pos in activeNeighs and (
+        activeNeighs[pos] == 3 or (
+            pos in active and activeNeighs[pos] == 2
+        )
+    )
+
+
+for c in range(6):
+    newActive = []
+    activeNeighs = {}
+    for pos in active:
+        for ne in neighbours(pos):
+            if ne not in activeNeighs:
+                activeNeighs[ne] = 1
+            else:
+                activeNeighs[ne] += 1
+    for pos in activeNeighs:
+        if isAlive(pos):
+            newActive.append(pos)
+    active = newActive
+
+result = len(active)
+
+with open("output1.txt", "w") as output:
+    output.write(str(result))
+    print(str(result))
