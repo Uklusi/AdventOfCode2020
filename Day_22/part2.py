e8a0c657cbe274dc17d114c90ae070caa7792c28
Manuele Cusumano <mcusumano@KPMG.IT>
2020-12-22 15:32:57 +0100

    Quicker part 2

--- Day_22/part2.py
+++ Day_22/part2.py
@@ -1,59 +1,59 @@
+from collections import deque
+
 result = 0
 p1 = []
 p2 = []
 
-
-def popLeft(queue):
-    val = queue[0]
-    queue.remove(queue[0])
-    return val
-
-
 with open("input.txt", "r") as input:
     fileContent = input.read().strip()
     p1String, p2String = fileContent.split("\n\n")
-    p1 = [int(n) for n in p1String.split("\n")[1:]]
-    p2 = [int(n) for n in p2String.split("\n")[1:]]
+    p1 = deque([int(n) for n in p1String.split("\n")[1:]])
+    p2 = deque([int(n) for n in p2String.split("\n")[1:]])
 
 
-def recursiveCombatRound(p1, p2):
-    p1Val = popLeft(p1)
-    p2Val = popLeft(p2)
-    if len(p1) < p1Val or len(p2) < p2Val:
-        if p1Val > p2Val:
-            p1.append(p1Val)
-            p1.append(p2Val)
-        else:
-            p2.append(p2Val)
-            p2.append(p1Val)
-    else:
-        subGameWinner = recursiveCombat(p1[:p1Val], p2[:p2Val])[0]
-        if subGameWinner == 1:
-            p1.append(p1Val)
-            p1.append(p2Val)
-        else:
-            p2.append(p2Val)
-            p2.append(p1Val)
-    return (p1, p2)
+def copyToN(queue, n):
+    i = iter(queue)
+    d = deque()
+    for _ in range(n):
+        d.append(next(i))
+    return d
 
 
 def recursiveCombat(p1, p2):
     states = set()
     while True:
-        state = ",".join([str(n) for n in p1]) + "|" + ",".join([str(n) for n in p2])
+        state = (tuple(p1), tuple(p2))
         if state in states:
-            return (1, p1, p2)
+            return (1, p1)
         states.add(state)
         if len(p1) == 0:
-            return (2, p1, p2)
+            return (2, p2)
         elif len(p2) == 0:
-            return (1, p1, p2)
-        (p1, p2) = recursiveCombatRound(p1, p2)
+            return (1, p1)
+
+        p1Val = p1.popleft()
+        p2Val = p2.popleft()
+        if len(p1) < p1Val or len(p2) < p2Val:
+            if p1Val > p2Val:
+                p1.append(p1Val)
+                p1.append(p2Val)
+            else:
+                p2.append(p2Val)
+                p2.append(p1Val)
+        else:
+            (subGameWinner, _) = recursiveCombat(
+                copyToN(p1, p1Val),
+                copyToN(p2, p2Val)
+            )
+            if subGameWinner == 1:
+                p1.append(p1Val)
+                p1.append(p2Val)
+            else:
+                p2.append(p2Val)
+                p2.append(p1Val)
 
 
-res = recursiveCombat(p1, p2)
-
-winner = res[res[0]]
+(_, winner) = recursiveCombat(p1, p2)
 
 for i in range(1, len(winner) + 1):
     n = winner.pop()
