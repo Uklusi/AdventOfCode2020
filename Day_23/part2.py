260e41c78c6b7d28b2addd532f97d88e78ffe668
Manuele Cusumano <mcusumano@KPMG.IT>
2020-12-23 13:14:47 +0100

    Faster Part 2
    Got rid of the data structure, now it's 7s

--- Day_23/part2.py
+++ Day_23/part2.py
@@ -1,92 +1,43 @@
 result = 0
 MAXCUP = 1000000
 MAXITER = 10000000
-
-
-class Node:
-    def __init__(self, data):
-        self.data = data
-        self.next = None
-        self.prev = None
-
-    def __repr__(self):
-        return str(self.data)
-
-    def __eq__(self, node):
-        return self.data == node.data
-
-    def __ne__(self, node):
-        return not self == node
-
-
-class LinkedList:
-    def __init__(self, nodes=None):
-        self.head = None
-        self.tail = None
-        if nodes is not None:
-            node = Node(data=nodes.pop(0))
-            self.head = node
-            for elem in nodes:
-                node.next = Node(data=elem)
-                prevnode = node
-                node = node.next
-                node.prev = prevnode
-            node.next = self.head
-            self.tail = node
-            self.head.prev = node
-
-    def __repr__(self):
-        node = self.head
-        nodes = []
-        while node is not None:
-            nodes.append(node.data)
-            node = node.next
-        nodes.append("None")
-        return " -> ".join(nodes)
-
-    def __iter__(self):
-        if self.head is not None:
-            yield self.head
-            node = self.head.next
-            while node is not None and node != self.head:
-                yield node
-                node = node.next
+MAXCUPININPUT = 9
+INPUTL = MAXCUPININPUT + 1
 
 
 with open("input.txt", "r") as input:
     fileContent = input.read().strip()
     cups = [int(n) for n in fileContent]
 
-cups += list(range(max(cups) + 1, MAXCUP + 1))
+nextVals = [0] + [
+    INPUTL if i == cups[-1] else cups[cups.index(i) + 1]
+    for i in range(1, INPUTL)
+]
 
-cups = LinkedList(nodes=cups)
+nextVals.extend(range(INPUTL + 1, MAXCUP + 1))
+nextVals.append(cups[0])
 
-nodes = [node for node in cups]
-nodes.sort(key=(lambda node: node.data))
-
-nodes = [None] + nodes
-
-currCup = cups.head
+currNode = cups[0]
 for i in range(MAXITER):
-    crab = [currCup.next, currCup.next.next, currCup.next.next.next]
-    n = currCup.data - 1
-    while n <= 0 or nodes[n] in crab:
-        n = n - 1
-        if n <= 0:
+    a = nextVals[currNode]
+    b = nextVals[a]
+    c = nextVals[b]
+    nextVals[currNode] = nextVals[c]
+    n = currNode - 1
+    while n in (0, a, b, c):
+        if n == 0:
             n = MAXCUP
-    crab[0].prev.next = crab[2].next
-    crab[2].next.prev = crab[0].prev
-    nbefore = nodes[n]
-    nafter = nbefore.next
-    nbefore.next = crab[0]
-    crab[0].prev = nbefore
-    nafter.prev = crab[2]
-    crab[2].next = nafter
-    currCup = currCup.next
+        else:
+            n -= 1
+    x = nextVals[n]
+    nextVals[n] = a
+    nextVals[c] = x
+    currNode = nextVals[currNode]
 
-i1 = nodes[1]
+a = nextVals[1]
+b = nextVals[a]
 
-result = i1.next.data * i1.next.next.data
+result = a * b
 
 with open("output2.txt", "w") as output:
     output.write(str(result))
